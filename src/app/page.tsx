b1c3b146e1577937eb07fe6c2decfde179c42256
bielvelozo <89873855+bielvelozo@users.noreply.github.com>
2023-09-07 21:31:34 -0300

    crud working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,24 @@
  'use client'
  import Layout from "@/components/Layout";
  import Table from "@/components/Table";
- import Client from "@/core/Client";
  import Button from "@/components/Button"
  import Form from "@/components/Form";
-import { useState , useEffect} from "react";
- import ClientRepository from "@/core/ClientRepository";
- import ClientCollection from "@/backend/db/ClientCollection";
+import useClients from "@/hooks/useClients";
+
 
 
 
 
 
 export default function Home() {
-   const repo: ClientRepository = new ClientCollection()
-
-   const [visible, setVisible] = useState<'table' | 'form'>('table')
-   const [clients, setClients] = useState<Client[]>([])
-   const [client, setClient] = useState<Client>(Client.void())
-  
-  //  const clients = [
-  //    new Client('biel', 18, '1'),
-  //    new Client('Alan', 39, '2'),
-  //    new Client('Celia', 51, '3'),
-  //    new Client('Sofia', 24, '4'),
-  //  ]
-
-   function selectClient(client: Client) {
-     setClient(client)
-     setVisible('form')
-   }
-   function deleteClient(client: Client) {
-     console.log(client.name)
-   }
-
-   function newClient() {
-     setClient(Client.void())
-     setVisible('form')
-   }
-  
-   function saveClient(client: Client) {
-     console.log(client)
-     setVisible('table')
-   }
+  const { selectClient, 
+          getAll, 
+          newClient, 
+          saveClient, 
+          deleteClient,
+          client,
+          clients
+        } = useClients()
 
   return (
     <div className={`
