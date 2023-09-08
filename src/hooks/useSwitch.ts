a790136d51634d2d2fc9ed5cf37eb7a5b1702dca
bielvelozo <89873855+bielvelozo@users.noreply.github.com>
2023-09-07 21:40:40 -0300

    refactor: switch form and table

---
+++ src/hooks/useSwitch.ts
@@ -0,0 +1,11 @@
+import { useState } from "react"
+export default function useSwitch() {
+    const [visible, setVisible] = useState<'table' | 'form'>('table')
+
+    const showTable = () => setVisible('table')
+    const showForm = () => setVisible('form')
+    return {
+        formVisible: visible === 'form',
+        tableVisible: visible === 'table'
+    }
+}
